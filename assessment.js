44d7104ca23b35d659245135f0abe6b82b11c6ec
Gunnar Huscroft <gunnarhuscroft@Gunnars-MacBook-Pro.local>
2018-11-01 17:23:49 -0600

    removing wrong answers

--- assessment.js
+++ assessment.js
@@ -8,11 +8,11 @@ var faveNum = 1 ;
 
 // #3 create a variable called 'lovesCode' and set it to true
 
-var lovesCode = true
+//come back to this one..
 
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
 
-var me = My name is (myName)
+//and this one.. reveiw notes
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
